488b50b5179dc483e0fd63f3e85af85fff9cd981
Rajeswari Palacharla <74202110+Rajeswari0410@users.noreply.github.com>
2026-03-30 16:53:27 -0500

    Modify views to include urban status and limit check
    Added is_urban and exceeded_recommended_limit fields to views.

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -2,7 +2,7 @@
 IF OBJECT_ID('gold.dim_kids', 'V') IS NOT NULL
 	DROP VIEW gold.dim_kids
 GO
---create object - view		
+
 CREATE VIEW gold.dim_kids AS
 SELECT 
 	ROW_NUMBER() OVER (ORDER BY age, gender, primary_device, health_impacts, urban_or_rural) AS kid_key, --PK
@@ -10,18 +10,23 @@ SELECT
 	gender,
 	primary_device,
 	health_impacts,
-	urban_or_rural
+	CASE WHEN urban_or_rural = 'Urban' THEN 1
+		ELSE 0
+	END AS is_urban
 FROM silver.kids_screen_time;
 
 IF OBJECT_ID('gold.fact_kids', 'V') IS NOT NULL
 	DROP VIEW gold.fact_kids
 GO
+
 --create object - facts
 CREATE VIEW gold.fact_kids AS
 SELECT 
 	dk.kid_key,
 	kst.avg_daily_screen_time_hr,
-	kst.exceeded_recommended_limit,
+	CASE WHEN 	kst.exceeded_recommended_limit = 'TRUE' THEN 1
+		ELSE 0
+	END AS exceeded_recommended_limit,
 	kst.educational_to_recreational_ratio
 FROM silver.kids_screen_time kst
 LEFT JOIN gold.dim_kids dk
@@ -29,4 +34,4 @@ ON kst.age = dk.age
  AND kst.gender = dk.gender
  AND kst.primary_device = dk.primary_device
  AND kst.health_impacts = dk.health_impacts
- AND kst.urban_or_rural = dk.urban_or_rural;
+ AND CASE WHEN kst.urban_or_rural = 'Urban' THEN 1 ELSE 0 END = dk.is_urban;
